974b97181b2aa882ede2bbe8190fdeeaeacc5a18
Minal <minal.hatekar@tcs.com>
2023-02-06 03:35:19 +0000

    Day17(Assignment)

---
+++ Day17/hospital-app/rabbit-mq-hospital-app-producer/src/main/java/com/minal/app/service/RabbitMQSender.java
@@ -0,0 +1,30 @@
+package com.minal.app.service;
+
+import com.minal.app.model.Hospital;
+import com.minal.app.model.Patient;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQSender {
+  @Autowired
+  private AmqpTemplate rabbitTemplate;
+
+  @Value("${rabbitmq.exchange.name}")
+  private String exchange;
+
+  @Value("${rabbitmq.routingkey.name}")
+  private String routingkey;
+
+  public void send(Hospital hospital) {
+    rabbitTemplate.convertAndSend(exchange, routingkey, hospital);
+    System.out.println("Send msg = " + hospital);
+  }
+
+  public void sendP(Patient patient){
+    rabbitTemplate.convertAndSend(exchange,routingkey, patient);
+    System.out.println("data send : " + patient);
+  }
+}
